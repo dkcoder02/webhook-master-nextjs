56a09c6b08b570d9c27f86d4a7574f206f1dcd86
KD <162318672+dkcoder02@users.noreply.github.com>
2024-11-30 13:42:50 +0000

    implement role based acess control and sign in functionality

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,48 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createClerkClient, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const publicRoutes = createRouteMatcher(["/", "/api/webhook/register", "/sign-in", "/sign-up"]);
+
+const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
+
+export default clerkMiddleware(async (auth, req) => {
+    const { userId } = await auth()
+
+    if (!userId && !publicRoutes(req)) {
+        return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+
+    if (userId) {
+        try {
+            const user = await clerkClient.users.getUser(userId);
+            const role = user.publicMetadata.role as string | undefined;
+
+            // Admin role redirection logic
+            if (role === "admin" && req.nextUrl.pathname === "/dashboard") {
+                return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+            }
+
+            // Prevent non-admin users from accessing admin routes
+            if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+                return NextResponse.redirect(new URL("/dashboard", req.url));
+            }
+
+            // Redirect authenticated users trying to access public routes
+            if (publicRoutes(req)) {
+                return NextResponse.redirect(
+                    new URL(
+                        role === "admin" ? "/admin/dashboard" : "/dashboard",
+                        req.url
+                    )
+                );
+            }
+        } catch (error) {
+            console.error("Error fetching user data from Clerk:", error);
+            return NextResponse.redirect(new URL("/error", req.url));
+        }
+    }
+})
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
